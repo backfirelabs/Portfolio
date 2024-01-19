e4d7ec1334ee03ac8e3835eabf540267bbbc86b9
Prosilentgamer2 <fearlesstechofficial@gmail.com>
2024-01-19 15:18:41 -0600

    V1

---
+++ testing.js
@@ -0,0 +1,10 @@
+// Name: Tanner Terrio
+// Date: 01/09/24
+
+let myNumber = 5;
+myNumber = myNumber + 3; // Perform a mathematical operation!
+
+let favoriteColor = prompt("What is your favorite color?");
+
+console.log("The value of myNumber is: " + myNumber);
+console.log("The favorite color is: " + favoriteColor);
