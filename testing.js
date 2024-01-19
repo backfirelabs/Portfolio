9daf2e29174f111354879dbecb99e582ae123b4d
Prosilentgamer2 <fearlesstechofficial@gmail.com>
2024-01-19 16:32:56 -0600

    Updated testing.js to add Loops and Conditions

--- testing.js
+++ testing.js
@@ -1,10 +1,27 @@
 // Name: Tanner Terrio
 // Date: 01/09/24
 
-let myNumber = 5;
-myNumber = myNumber + 3; // Perform a mathematical operation!
+// let myNumber = 5;
+// myNumber = myNumber + 3; // Perform a mathematical operation!
 
-let favoriteColor = prompt("What is your favorite color?");
+// let favoriteColor = prompt("What is your favorite color?");
 
-console.log("The value of myNumber is: " + myNumber);
-console.log("The favorite color is: " + favoriteColor);
+// console.log("The value of myNumber is: " + myNumber);
+// console.log("The favorite color is: " + favoriteColor);
+
+
+// Loops and conditionls
+
+// Initialize the variable
+let startingNumber = 7; // You can choose any starting number
+
+// Create a for loop that iterates 5 times
+for (let i = 0; i < 5; i++) {
+    // Check if the number is odd
+    if (startingNumber % 2 === 1) {
+        console.log(startingNumber); // Print the number if it's odd
+    }
+
+    // Increment the number by 1
+    startingNumber += 1;
+}
