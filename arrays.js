4fff3621be0d29aacc1ba9584df3132ac3a27ef1
Prosilentgamer2 <fearlesstechofficial@gmail.com>
2024-01-28 15:02:19 -0600

    Formatting

--- arrays.js
+++ arrays.js
@@ -2,20 +2,20 @@
 let myArray = [10, 21, 32, 43];
 
 // Function expression that adds 1 and logs to the console
-let addOne = function(num) {
-    console.log(num + 1);
+let addOne = function (num) {
+  console.log(num + 1);
 };
 
 // Function that takes a function expression and a number
 function processNumber(funcExpression, number) {
-    if (number % 2 === 0) {
-        funcExpression(number);
-    } else {
-        console.log("The number is odd");
-    }
+  if (number % 2 === 0) {
+    funcExpression(number);
+  } else {
+    console.log("The number is odd");
+  }
 }
 
 //  For-in loop through the array
 for (let index in myArray) {
-    processNumber(addOne, myArray[index]);
+  processNumber(addOne, myArray[index]);
 }
